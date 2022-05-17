1fc2f77c340bb11db7f46f1ed5ce70ddc097f797
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-05-17 16:08:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header-layout></header-layout>
+  </div>
+</template>
+
+<script>
+import HeaderLayout from './components/HeaderLayout.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderLayout
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/HeaderLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "HeaderLayout"
+}
+</script>
+
+<style>
+
+</style>
